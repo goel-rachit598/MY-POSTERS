5bde4e0ce31be8a6f80356d0a6398042a7db04bf
rachitgoel05 <43531612+rachitgoel05@users.noreply.github.com>
2019-08-24 10:20:22 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+left = [0]*n 
+right = [0]*n  
+prod = [0]*n  
+left[0] = 1
+right[n - 1] = 1 
+for i in range(1, n): 
+    left[i] = arr[i - 1] * left[i - 1]  
+for j in range(n-2, -1, -1): 
+    right[j] = arr[j + 1] * right[j + 1] 
+for i in range(n): 
+    prod[i] = left[i] * right[i] 
+for i in range(n): 
+    print(prod[i], end=' ') 
